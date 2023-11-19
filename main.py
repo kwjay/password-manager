51080bead6000ed4ad5e01c24dd64b8241180da9
kwjay <kwjay1002@gmail.com>
2023-11-19 21:02:57 +0100

    edited positions and sizes of canvas

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from tkinter import *
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
-canvas = Canvas(window)
+canvas = Canvas(height=200, width=200)
 logo_image = PhotoImage(file="logo.png")
-image = canvas.create_image(150, 150, image=logo_image)
+image = canvas.create_image(100, 100, image=logo_image)
 canvas.pack()
 
 window.mainloop()
